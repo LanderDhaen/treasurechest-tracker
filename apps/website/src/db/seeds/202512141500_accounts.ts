e878001e4cf0ac21d60fe75be26994e7d6c80f78
LanderDhaen <lander.dhaen@student.hogent.be>
2025-12-31 16:49:41 +0100

    feat(seeding): Improved seeding for clans and accounts

--- apps/website/src/db/seeds/202512141500_accounts.ts
+++ apps/website/src/db/seeds/202512141500_accounts.ts
@@ -4,18 +4,6 @@ import { Kysely } from "kysely";
 export const seed = async (db: Kysely<any>) => {
   await db.deleteFrom("account").execute();
 
-  const dl3 = await db
-    .selectFrom("clan")
-    .select(["id"])
-    .where("tag", "=", "28UYR0CVU")
-    .executeTakeFirstOrThrow();
-
-  const dlcw = await db
-    .selectFrom("clan")
-    .select(["id"])
-    .where("tag", "=", "29RPVGYU8")
-    .executeTakeFirstOrThrow();
-
   await db
     .insertInto("account")
     .values([
@@ -25,23 +13,7 @@ export const seed = async (db: Kysely<any>) => {
         name: "DL✨Lander",
         tag: "8RRG0LJR2",
         townhall: 18,
-        clanId: dl3.id,
-      },
-      {
-        createdAt: "2025-12-14 15:00:00",
-        updatedAt: "2025-12-14 15:00:00",
-        name: "DL✨Lander™",
-        tag: "PVPV2U0JG",
-        townhall: 17,
-        clanId: dlcw.id,
-      },
-      {
-        createdAt: "2025-12-14 15:00:00",
-        updatedAt: "2025-12-14 15:00:00",
-        name: "Lander",
-        tag: "9UQGJYLCV",
-        townhall: 17,
-        clanId: dlcw.id,
+        clanId: 1, // Dutch Legion 3
       },
       {
         createdAt: "2025-12-14 15:00:00",
@@ -49,7 +21,23 @@ export const seed = async (db: Kysely<any>) => {
         name: "DL Senne",
         tag: "L00PRVC",
         townhall: 17,
-        clanId: dlcw.id,
+        clanId: 2, // Dutch Legion CW
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "DL✨Lander™",
+        tag: "PVPV2U0JG",
+        townhall: 17,
+        clanId: 3, // Dutch Legion 4
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "Lander",
+        tag: "9UQGJYLCV",
+        townhall: 17,
+        clanId: 3, // Dutch Legion 4
       },
       {
         createdAt: "2025-12-14 15:00:00",
@@ -57,7 +45,95 @@ export const seed = async (db: Kysely<any>) => {
         name: "DL✨Senne",
         tag: "YVCLRY98U",
         townhall: 17,
-        clanId: dlcw.id,
+        clanId: 3, // Dutch Legion 4
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "Lander™",
+        tag: "8UQ9LGYQ",
+        townhall: 16,
+        clanId: 4, // DL Mini
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "SyNx_Viiper",
+        tag: "ULP0Y98C",
+        townhall: 15,
+        clanId: 3, // Dutch Legion 4
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "DL Lander",
+        tag: "G2V990J",
+        townhall: 14,
+        clanId: 3, // Dutch Legion 4
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "Lvl.1 Lander",
+        tag: "G2P0LP0LL",
+        townhall: 12,
+        clanId: 3, // Dutch Legion 4
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "FC Gods",
+        tag: "PULUP0L9",
+        townhall: 11,
+        clanId: 4, // DL Mini
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "EXP Flickzie",
+        tag: "88G9UPCU",
+        townhall: 10,
+        clanId: 4, // DL Mini
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "Peace",
+        tag: "28U8RUVL",
+        townhall: 9,
+        clanId: 4, // DL Mini
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "Lvl.9 Lander",
+        tag: "PUCV28QP9",
+        townhall: 9,
+        clanId: 4, // DL Mini
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "Lander - TH8",
+        tag: "YGJL820VP",
+        townhall: 8,
+        clanId: 4, // DL Mini
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "Lvl.8 Lander",
+        tag: "QY0RVPYQ",
+        townhall: 8,
+        clanId: 4, // DL Mini
+      },
+      {
+        createdAt: "2025-12-14 15:00:00",
+        updatedAt: "2025-12-14 15:00:00",
+        name: "Lvl.7 Lander",
+        tag: "LYC0RRUUL",
+        townhall: 7,
+        clanId: 4, // DL Mini
       },
     ])
     .execute();
