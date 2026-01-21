075b0e5e860707b1abf99f47e8a0f587437e691c
LanderDhaen <lander.dhaen@student.hogent.be>
2026-01-21 17:14:04 +0100

    refactor(event-sorting): Moved the event table to a seperate component

--- apps/website/src/app/events/(overview)/page.tsx
+++ apps/website/src/app/events/(overview)/page.tsx
@@ -1,25 +1,13 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-
 import { getAllEvents } from "@/actions/event";
-import StatusBadge from "@/components/status-badge";
-import { formatDate } from "@/lib/utils";
-import GiftBadge from "@/components/gift-badge";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import Pagination from "@/components/pagination";
+import EventTable from "@/components/event-table";
 
 export default async function Page({
   searchParams,
@@ -43,55 +31,14 @@ export default async function Page({
           {`Currently tracking ${count} event${count !== 1 ? "s" : ""}.`}
         </CardDescription>
       </CardHeader>
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>#</TableHead>
-              <TableHead>Name</TableHead>
-              <TableHead>Start Date</TableHead>
-              <TableHead>End Date</TableHead>
-              <TableHead>Rewards</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {events.length === 0 ? (
-              <TableRow>
-                <TableCell colSpan={5} className="text-center italic">
-                  No events found.
-                </TableCell>
-              </TableRow>
-            ) : (
-              events.map((event) => (
-                <TableRow key={event.id}>
-                  <TableCell>{event.id}</TableCell>
-                  <TableCell>
-                    <div className="flex items-center gap-2">
-                      <StatusBadge status={event.status} />
-                      {event.name}
-                      <GiftBadge isGift={event.isGift} />
-                    </div>
-                  </TableCell>
-                  <TableCell>{formatDate(event.startDate)}</TableCell>
-                  <TableCell>{formatDate(event.endDate)}</TableCell>
-                  <TableCell>
-                    {event.maxChests == 1
-                      ? "1 Chest"
-                      : `${event.maxChests} Chests`}
-                  </TableCell>
-                </TableRow>
-              ))
-            )}
-          </TableBody>
-        </Table>
-      </CardContent>
-      <CardFooter className="justify-end">
+      <CardContent className="flex flex-col gap-4">
+        <EventTable events={events} />
         <Pagination
           currentPage={page}
           currentPageSize={pageSize}
           totalPages={totalPages}
         />
-      </CardFooter>
+      </CardContent>
     </Card>
   );
 }
---
+++ apps/website/src/components/event-table.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { formatDate } from "@/lib/utils";
+import GiftBadge from "./gift-badge";
+import StatusBadge from "./status-badge";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+import { EventStatus } from "@/types/event-status";
+
+interface Event {
+  id: number;
+  status: EventStatus;
+  name: string;
+  isGift: boolean;
+  startDate: Date;
+  endDate: Date;
+  maxChests: number;
+}
+
+interface EventTableProps {
+  events: Event[];
+}
+
+export default function EventTable({ events }: EventTableProps) {
+  return (
+    <div className="rounded-md border overflow-hidden">
+      <Table>
+        <TableHeader className="bg-muted">
+          <TableRow>
+            <TableHead className="font-bold w-12">#</TableHead>
+            <TableHead className="font-bold">Name</TableHead>
+            <TableHead className="font-bold">Start Date</TableHead>
+            <TableHead className="font-bold">End Date</TableHead>
+            <TableHead className="font-bold">Rewards</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {events.length === 0 ? (
+            <TableRow>
+              <TableCell colSpan={5} className="text-center italic">
+                No events found.
+              </TableCell>
+            </TableRow>
+          ) : (
+            events.map((event) => (
+              <TableRow key={event.id}>
+                <TableCell>{event.id}</TableCell>
+                <TableCell>
+                  <div className="flex items-center gap-2">
+                    <StatusBadge status={event.status} />
+                    {event.name}
+                    <GiftBadge isGift={event.isGift} />
+                  </div>
+                </TableCell>
+                <TableCell>{formatDate(event.startDate)}</TableCell>
+                <TableCell>{formatDate(event.endDate)}</TableCell>
+                <TableCell>
+                  {event.maxChests == 1
+                    ? "1 Chest"
+                    : `${event.maxChests} Chests`}
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
