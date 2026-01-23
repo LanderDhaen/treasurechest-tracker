d733222aa19163e8d89e6de4063edf77a4316116
LanderDhaen <lander.dhaen@student.hogent.be>
2026-01-23 19:31:05 +0100

    feat(event-sorting): Moved account sorting constants to seperate folder

---
+++ apps/website/src/constants/account.ts
@@ -0,0 +1,8 @@
+export const SORT_OPTIONS = [
+  { label: "Townhall", value: "townhall" },
+  { label: "Name", value: "name" },
+  { label: "Clan", value: "clan" },
+];
+
+export const DEFAULT_SORT_OPTION = "name";
+export const DEFAULT_SORT_DIRECTION = "asc";
--- apps/website/src/schemas/account.ts
+++ apps/website/src/schemas/account.ts
@@ -1,10 +1,14 @@
 import * as z from "zod";
 import { paginationSearchParamsSchema } from "./common";
+import {
+  DEFAULT_SORT_DIRECTION,
+  DEFAULT_SORT_OPTION,
+} from "@/constants/account";
 
 export const accountSearchParamsSchema = z.object({
   ...paginationSearchParamsSchema.shape,
-  sortBy: z.enum(["townhall", "name", "clan"]).catch("name"),
-  direction: z.enum(["asc", "desc"]).catch("asc"),
+  sortBy: z.enum(["townhall", "name", "clan"]).catch(DEFAULT_SORT_OPTION),
+  direction: z.enum(["asc", "desc"]).catch(DEFAULT_SORT_DIRECTION),
 });
 
 export type AccountSearchParams = z.infer<typeof accountSearchParamsSchema>;
