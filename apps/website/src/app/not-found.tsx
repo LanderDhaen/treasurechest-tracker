9ddd8850a42cebd5c507ef90b30bb63cf063f937
LanderDhaen <lander.dhaen@student.hogent.be>
2026-03-01 23:46:48 +0100

    feat(dashboard-rework): Implemented global not found

---
+++ apps/website/src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <Empty>
+      <EmptyHeader>
+        <EmptyTitle>404 - Not Found</EmptyTitle>
+        <EmptyDescription>
+          The page you&apos;re looking for doesn&apos;t exist.
+        </EmptyDescription>
+      </EmptyHeader>
+      <EmptyContent>
+        <EmptyDescription>
+          <Link href="/">View the dashboard</Link>
+        </EmptyDescription>
+      </EmptyContent>
+    </Empty>
+  );
+}
