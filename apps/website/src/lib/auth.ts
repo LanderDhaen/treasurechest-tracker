a4de056903a25ac7549571e0fe4de17e7e981192
LanderDhaen <lander.dhaen@student.hogent.be>
2026-03-03 22:56:09 +0100

    fix(auth): Fixed issue where columns of auth tables were camel case

--- apps/website/src/lib/auth.ts
+++ apps/website/src/lib/auth.ts
@@ -1,15 +1,12 @@
+import { db } from "@/db";
 import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
-import { Pool } from "pg";
 
 export const auth = betterAuth({
-  database: new Pool({
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-  }),
+  database: {
+    db: db,
+    type: "postgres",
+  },
   account: {
     modelName: "profile",
   },
