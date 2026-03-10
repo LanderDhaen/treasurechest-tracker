4c634a2fe283936a2ee93c3c01e00fd440f0d6a1
LanderDhaen <lander.dhaen@student.hogent.be>
2026-03-10 16:50:13 +0100

    feat(account-form): Added routing to form page

---
+++ apps/website/src/components/auth-guard.tsx
@@ -0,0 +1,15 @@
+import { getServerSession } from "@/actions/auth";
+
+export default async function AuthGuard({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerSession();
+
+  if (!session) {
+    return null;
+  }
+
+  return <>{children}</>;
+}
