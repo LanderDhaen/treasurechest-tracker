b27b28f68038c079e85495a42de4632c840bfa32
LanderDhaen <lander.dhaen@student.hogent.be>
2026-03-16 23:07:46 +0100

    feat(event-actions): Added metadata columns to event table

--- apps/website/src/db/migrations/202512180100_createEventTable.ts
+++ apps/website/src/db/migrations/202512180100_createEventTable.ts
@@ -5,6 +5,9 @@ export const up = async (db: Kysely<any>) => {
   await db.schema
     .createTable("event")
     .addColumn("id", "serial", (c) => c.primaryKey())
+    .addColumn("createdAt", "timestamp", (c) => c.notNull().defaultTo("now()"))
+    .addColumn("updatedAt", "timestamp", (c) => c.notNull().defaultTo("now()"))
+    .addColumn("isActive", "boolean", (c) => c.notNull().defaultTo(true))
     .addColumn("edition", "integer", (c) => c.notNull())
     .addColumn("code", "varchar", (c) => c.notNull().unique())
     .addColumn("startDate", "date", (c) => c.notNull())
