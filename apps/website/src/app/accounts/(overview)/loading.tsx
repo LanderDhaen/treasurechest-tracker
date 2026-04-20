a81622bf1d606f05dac4940f63a82a4642a57ee8
LanderDhaen <lander.dhaen@student.hogent.be>
2026-04-20 18:00:10 +0200

    refactor(loading): Moved the table skeleton to a reusable component

--- apps/website/src/app/accounts/(overview)/loading.tsx
+++ apps/website/src/app/accounts/(overview)/loading.tsx
@@ -1,4 +1,5 @@
 import PaginationSkeleton from "@/components/skeletons/pagination-skeleton";
+import TableSkeleton from "@/components/skeletons/table-skeleton";
 import ToolbarSkeleton from "@/components/skeletons/toolbar-skeleton";
 import {
   Card,
@@ -8,14 +9,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 
 export default function Loading() {
   return (
@@ -28,26 +21,7 @@ export default function Loading() {
       </CardHeader>
       <CardContent className="flex flex-col gap-4">
         <ToolbarSkeleton />
-        <div className="rounded-md border overflow-hidden">
-          <Table>
-            <TableHeader>
-              <TableRow className="bg-muted">
-                <TableHead colSpan={6} className="text-center">
-                  <div className="w-full h-5.5" />
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {[...Array(5)].map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell colSpan={6}>
-                    <Skeleton className="w-full h-5.5" />
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </div>
+        <TableSkeleton />
         <PaginationSkeleton />
       </CardContent>
     </Card>
