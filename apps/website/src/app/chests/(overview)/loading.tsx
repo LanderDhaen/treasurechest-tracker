1f4359b8ccabdaad675b30bcd19ab122c45d76c5
LanderDhaen <lander.dhaen@student.hogent.be>
2026-04-20 17:46:05 +0200

    feat(loading): Added loading state for chest tables

---
+++ apps/website/src/app/chests/(overview)/loading.tsx
@@ -0,0 +1,56 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function Loading() {
+  return (
+    <Card className="shadow-md">
+      <CardHeader>
+        <CardTitle>Treasure Chests</CardTitle>
+        <CardDescription>
+          <Skeleton className="w-full md:w-86 h-5" />
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-4">
+        <Skeleton className="w-full h-9" />
+        <div className="rounded-md border overflow-hidden">
+          <Table>
+            <TableHeader>
+              <TableRow className="bg-muted">
+                <TableHead colSpan={6} className="text-center">
+                  <div className="w-full h-5.5" />
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {[...Array(5)].map((_, index) => (
+                <TableRow key={index}>
+                  <TableCell colSpan={6}>
+                    <Skeleton className="w-full h-5.5" />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+        <div className="flex flex-col items-end md:flex-row md:items-center md:justify-end gap-4">
+          <Skeleton className="w-52 h-9" />
+          <Skeleton className="w-72 h-9" />
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
