56ee63026f309861c7a007e8abd8d7ff04e24db4
LanderDhaen <lander.dhaen@student.hogent.be>
2026-04-20 22:30:17 +0200

    refactor(loading): Moved timeline skeleton to separate component

--- apps/website/src/app/events/[code]/timeline/loading.tsx
+++ apps/website/src/app/events/[code]/timeline/loading.tsx
@@ -1,8 +1,7 @@
 import EventInformationItemSkeleton from "@/components/skeletons/event-information-item-skeleton";
 import TabsSkeleton from "@/components/skeletons/tabs-skeleton";
-import { Card, CardContent } from "@/components/ui/card";
+import TimelineSkeleton from "@/components/skeletons/timeline-skeleton";
 import { Separator } from "@/components/ui/separator";
-import { Spinner } from "@/components/ui/spinner";
 
 export default function Loading() {
   return (
@@ -10,14 +9,7 @@ export default function Loading() {
       <EventInformationItemSkeleton />
       <Separator />
       <TabsSkeleton />
-      <Card>
-        <CardContent className="h-100 flex gap-2 justify-center items-center">
-          <Spinner />
-          <span className="italic text-muted-foreground">
-            Loading timeline...
-          </span>
-        </CardContent>
-      </Card>
+      <TimelineSkeleton />
     </div>
   );
 }
