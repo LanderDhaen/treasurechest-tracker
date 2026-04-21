bd1c24c9bf08a09e45c00c44668b8b33f64b4b82
LanderDhaen <lander.dhaen@student.hogent.be>
2026-04-22 01:42:43 +0200

    feat(empty): Made sure visitors have a different empty state when there's no events yet

--- apps/website/src/components/no-events.tsx
+++ apps/website/src/components/no-events.tsx
@@ -9,8 +9,40 @@ import {
   EmptyMedia,
   EmptyTitle,
 } from "./ui/empty";
+import { getServerSession } from "@/queries/auth";
 
-export default function NoEvents() {
+export default async function NoEvents() {
+  const session = await getServerSession();
+
+  if (session) {
+    return (
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <CalendarX2 />
+          </EmptyMedia>
+          <EmptyTitle>Nothing here yet</EmptyTitle>
+          <EmptyDescription>
+            Add your first event and watch this space come to life.
+          </EmptyDescription>
+        </EmptyHeader>
+        <EmptyContent>
+          <Button variant="outline" asChild>
+            <Link
+              href={{
+                pathname: "/events/add",
+                query: { returnTo: "/events" },
+              }}
+            >
+              <CalendarPlus />
+              Add event
+            </Link>
+          </Button>
+        </EmptyContent>
+      </Empty>
+    );
+  }
+
   return (
     <Empty>
       <EmptyHeader>
@@ -19,22 +51,9 @@ export default function NoEvents() {
         </EmptyMedia>
         <EmptyTitle>Nothing here yet</EmptyTitle>
         <EmptyDescription>
-          Add your first event and watch this space come to life.
+          Lander hasn&apos;t added any events yet, check back later.
         </EmptyDescription>
       </EmptyHeader>
-      <EmptyContent>
-        <Button variant="outline" asChild>
-          <Link
-            href={{
-              pathname: "/events/add",
-              query: { returnTo: "/events" },
-            }}
-          >
-            <CalendarPlus />
-            Add event
-          </Link>
-        </Button>
-      </EmptyContent>
     </Empty>
   );
 }
