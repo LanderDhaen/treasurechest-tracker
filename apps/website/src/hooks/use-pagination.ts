d861a2a14137c68d32a4114ba3fe57c9a3d31c48
LanderDhaen <lander.dhaen@student.hogent.be>
2026-04-23 17:54:53 +0200

    feat(nuqs): Added scroll to top when changing pages

--- apps/website/src/hooks/use-pagination.ts
+++ apps/website/src/hooks/use-pagination.ts
@@ -2,12 +2,14 @@ import { parseAsInteger, useQueryStates } from "nuqs";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE } from "@/constants/common";
 
 export function usePagination() {
-  return useQueryStates({
-    page: parseAsInteger.withDefault(DEFAULT_PAGE).withOptions({
-      shallow: false,
-    }),
-    pageSize: parseAsInteger.withDefault(DEFAULT_PAGE_SIZE).withOptions({
-      shallow: false,
-    }),
-  });
+  return useQueryStates(
+    {
+      page: parseAsInteger.withDefault(DEFAULT_PAGE),
+      pageSize: parseAsInteger.withDefault(DEFAULT_PAGE_SIZE),
+    },
+    {
+      shallow: true,
+      scroll: true,
+    },
+  );
 }
