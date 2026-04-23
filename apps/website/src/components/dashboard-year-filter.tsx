943017462a1c19e40c2a450e4294617bc401e7a3
LanderDhaen <lander.dhaen@student.hogent.be>
2026-04-24 00:36:49 +0200

    feat(year-filter): Added ability to clear the select

--- apps/website/src/components/dashboard-year-filter.tsx
+++ apps/website/src/components/dashboard-year-filter.tsx
@@ -13,6 +13,9 @@ import {
 import { parseAsNumberLiteral, useQueryState } from "nuqs";
 
 import { RELEASE_YEAR } from "@/constants/event";
+import { ButtonGroup } from "./ui/button-group";
+import { Button } from "./ui/button";
+import { X } from "lucide-react";
 
 const CURRENT_YEAR = new Date().getFullYear();
 const YEARS = Array.from(
@@ -27,23 +30,34 @@ export default function DashboardYearFilter() {
   );
 
   return (
-    <Select
-      onValueChange={(value) => setSelectedYear(parseInt(value, 10))}
-      value={selectedYear?.toString()}
-    >
-      <SelectTrigger className="w-40 bg-white">
-        <SelectValue placeholder="Filter by year" />
-      </SelectTrigger>
-      <SelectContent position="popper">
-        <SelectGroup>
-          <SelectLabel>Year</SelectLabel>
-          {YEARS.map((year) => (
-            <SelectItem key={year} value={year.toString()}>
-              {year}
-            </SelectItem>
-          ))}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+    <ButtonGroup>
+      <Select
+        onValueChange={(value) => setSelectedYear(parseInt(value, 10))}
+        value={selectedYear?.toString() || ""}
+      >
+        <SelectTrigger className="w-40 bg-white">
+          <SelectValue placeholder="Filter by year" />
+        </SelectTrigger>
+        <SelectContent position="popper">
+          <SelectGroup>
+            <SelectLabel>Year</SelectLabel>
+            {YEARS.map((year) => (
+              <SelectItem key={year} value={year.toString()}>
+                {year}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Button
+        variant="outline"
+        size="icon"
+        className="bg-white hover:bg-white"
+        onClick={() => setSelectedYear(null)}
+        disabled={!selectedYear}
+      >
+        <X />
+      </Button>
+    </ButtonGroup>
   );
 }
