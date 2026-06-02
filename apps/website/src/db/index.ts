f97eb03c5ec347470efc170547925aa449862bcb
LanderDhaen <lander.dhaen@student.hogent.be>
2026-06-02 23:43:06 +0200

    refactor(v1): Replaced the seperate .env variables with DATABASE_URL

--- apps/website/src/db/index.ts
+++ apps/website/src/db/index.ts
@@ -8,11 +8,7 @@ types.setTypeParser(20, (val) => {
 
 export const dialect = new PostgresDialect({
   pool: new Pool({
-    host: process.env.DATABASE_HOST,
-    port: Number(process.env.DATABASE_PORT),
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    connectionString: process.env.DATABASE_URL,
   }),
 });
 
